a90de4db0c4fa7e977b64a0e2403ef76f4886dd8
Kazumune Katagiri <kkaz123@gmail.com>
2019-05-03 14:31:16 +0900

    static ファイルを表示できるようにした(EnableWebMvc が悪さをしてた)

--- src/main/kotlin/net/jp/vss/sample/VssApplication.kt
+++ src/main/kotlin/net/jp/vss/sample/VssApplication.kt
@@ -4,9 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-
 
 /**
  * Application Main.
@@ -33,7 +31,6 @@ fun main(args: Array<String>) {
  * CORS 用の設定.
  */
 @Configuration
-@EnableWebMvc
 class CorsConfiguration : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
