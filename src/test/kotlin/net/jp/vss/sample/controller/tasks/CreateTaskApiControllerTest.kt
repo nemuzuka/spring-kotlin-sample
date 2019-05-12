6225a0896fda1cfb6c94102e74d6b24244de2f63
Kazumune Katagiri <kkaz123@gmail.com>
2019-05-12 13:42:39 +0900

    OpenID Connect を有効にした時に Controller 側のテストが通らなくなったので、対応

--- src/test/kotlin/net/jp/vss/sample/controller/tasks/CreateTaskApiControllerTest.kt
+++ src/test/kotlin/net/jp/vss/sample/controller/tasks/CreateTaskApiControllerTest.kt
@@ -12,7 +12,8 @@ import org.hamcrest.CoreMatchers.`is`
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
@@ -27,7 +28,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
  * CreateTaskApiController のテスト.
  */
 @RunWith(SpringRunner::class)
-@WebMvcTest(CreateTaskApiController::class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class CreateTaskApiControllerTest {
 
     @Autowired
--- src/test/kotlin/net/jp/vss/sample/controller/tasks/ListTaskApiControllerTest.kt
+++ src/test/kotlin/net/jp/vss/sample/controller/tasks/ListTaskApiControllerTest.kt
@@ -9,7 +9,8 @@ import org.hamcrest.CoreMatchers.`is`
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.security.test.context.support.WithMockUser
@@ -23,7 +24,8 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
  * ListTaskApiController のテスト.
  */
 @RunWith(SpringRunner::class)
-@WebMvcTest(ListTaskApiController::class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class ListTaskApiControllerTest {
 
     @Autowired
